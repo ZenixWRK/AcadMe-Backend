33f5c98141196666690055f48cb687f64ef1076f
ZenixWRK <Zenixwrk@gmail.com>
2026-01-18 20:31:31 -0500

    Remove assignment route handlers

--- src/routes/assignmentsRoute.js
+++ src/routes/assignmentsRoute.js
@@ -10,22 +10,16 @@ import {
 
 const router = express.Router();
 
-// Create a new assignment
 router.post('/', createAssignment);
 
-// Delete an assignment by id
 router.delete('/:id', deleteAssignment);
 
-// Update an assignment by id
 router.put('/:id', updateAssignment);
 
-// Toggle assignment completion status
 router.patch('/:id/toggle', toggleAssignmentCompletion);
 
-// Get all assignments for a user
 router.get('/:userId', getAssignmentsByUserId);
 
-// Get assignments by subject for a user
 router.get('/:userId/subject/:subject', getAssignmentsBySubject);
 
 export default router;
